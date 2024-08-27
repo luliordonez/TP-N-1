c048d9fb1f02480aa79918ba21a9e36e3d47ad10
luliordonez <luciaordonez0605qgmail.com>
2024-08-26 22:55:48 -0300

    página final

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.getElementById("reservationForm").addEventListener("submit", function(event){
     event.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    const people = document.getElementById("people").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;
+    let people = document.getElementById("people").value;
 
-    const confirmationMessage = `Gracias, ${name}. Tu reserva para ${people} personas el día ${date} a las ${time} ha sido confirmada.`;
+    let confirmationMessage = `Gracias, ${name}. Tu reserva para ${people} personas el día ${date} a las ${time} ha sido confirmada.`;
     document.getElementById("confirmationMessage").innerText = confirmationMessage;
     
     document.getElementById("confirmation").style.display = "block";
